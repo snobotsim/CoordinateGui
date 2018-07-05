7e8f0e61ecb45e7a4a7247e34bb66f2bcf09f38b
PJ Reiniger <pj.reiniger@gmail.com>
2018-07-05 03:21:18 -0400

    Update code coverage
    Also prep for pulling in as a third party lib

--- CoordinateGuiStandalone/src/main/java/org/snobot/coordinate_gui/trajectory_gen/TrajectoryConfigPanel.java
+++ CoordinateGuiStandalone/src/main/java/org/snobot/coordinate_gui/trajectory_gen/TrajectoryConfigPanel.java
@@ -25,13 +25,24 @@ public class TrajectoryConfigPanel extends JPanel
         setConfig(new Config());
     }
 
-    public void setConfig(Config trajectoryConfig)
+    /**
+     * Sets the trajectory configuration.
+     * 
+     * @param aTrajectoryConfig
+     *            The configuration
+     */
+    public void setConfig(Config aTrajectoryConfig)
     {
-        mMaxVelocityField.setText(Double.toString(trajectoryConfig.max_vel));
-        mMaxAccelerationField.setText(Double.toString(trajectoryConfig.max_acc));
-        mMaxJerkField.setText(Double.toString(trajectoryConfig.max_jerk));
+        mMaxVelocityField.setText(Double.toString(aTrajectoryConfig.max_vel));
+        mMaxAccelerationField.setText(Double.toString(aTrajectoryConfig.max_acc));
+        mMaxJerkField.setText(Double.toString(aTrajectoryConfig.max_jerk));
     }
 
+    /**
+     * Gets the configuration represented in the panel.
+     * 
+     * @return The config
+     */
     public TrajectoryGenerator.Config getConfig()
     {
         double dt = .02;
@@ -56,68 +67,68 @@ public class TrajectoryConfigPanel extends JPanel
         setLayout(gridBagLayout);
 
         JLabel lblMaxVelocity = new JLabel("Max Velocity");
-        GridBagConstraints gbc_lblMaxVelocity = new GridBagConstraints();
-        gbc_lblMaxVelocity.anchor = GridBagConstraints.EAST;
-        gbc_lblMaxVelocity.insets = new Insets(0, 0, 5, 5);
-        gbc_lblMaxVelocity.gridx = 0;
-        gbc_lblMaxVelocity.gridy = 0;
-        add(lblMaxVelocity, gbc_lblMaxVelocity);
+        GridBagConstraints gbcLblMaxVelocity = new GridBagConstraints();
+        gbcLblMaxVelocity.anchor = GridBagConstraints.EAST;
+        gbcLblMaxVelocity.insets = new Insets(0, 0, 5, 5);
+        gbcLblMaxVelocity.gridx = 0;
+        gbcLblMaxVelocity.gridy = 0;
+        add(lblMaxVelocity, gbcLblMaxVelocity);
 
         mMaxVelocityField = new JTextField();
-        GridBagConstraints gbc_mMaxVelocityField = new GridBagConstraints();
-        gbc_mMaxVelocityField.insets = new Insets(0, 0, 5, 0);
-        gbc_mMaxVelocityField.fill = GridBagConstraints.HORIZONTAL;
-        gbc_mMaxVelocityField.gridx = 1;
-        gbc_mMaxVelocityField.gridy = 0;
-        add(mMaxVelocityField, gbc_mMaxVelocityField);
+        GridBagConstraints gbcMaxVelocityField = new GridBagConstraints();
+        gbcMaxVelocityField.insets = new Insets(0, 0, 5, 0);
+        gbcMaxVelocityField.fill = GridBagConstraints.HORIZONTAL;
+        gbcMaxVelocityField.gridx = 1;
+        gbcMaxVelocityField.gridy = 0;
+        add(mMaxVelocityField, gbcMaxVelocityField);
         mMaxVelocityField.setColumns(10);
 
         JLabel lblMaxAcceleration = new JLabel("Max Acceleration");
-        GridBagConstraints gbc_lblMaxAcceleration = new GridBagConstraints();
-        gbc_lblMaxAcceleration.anchor = GridBagConstraints.EAST;
-        gbc_lblMaxAcceleration.insets = new Insets(0, 0, 5, 5);
-        gbc_lblMaxAcceleration.gridx = 0;
-        gbc_lblMaxAcceleration.gridy = 1;
-        add(lblMaxAcceleration, gbc_lblMaxAcceleration);
+        GridBagConstraints gbcLblMaxAcceleration = new GridBagConstraints();
+        gbcLblMaxAcceleration.anchor = GridBagConstraints.EAST;
+        gbcLblMaxAcceleration.insets = new Insets(0, 0, 5, 5);
+        gbcLblMaxAcceleration.gridx = 0;
+        gbcLblMaxAcceleration.gridy = 1;
+        add(lblMaxAcceleration, gbcLblMaxAcceleration);
 
         mMaxAccelerationField = new JTextField();
-        GridBagConstraints gbc_mMaxAccelerationField = new GridBagConstraints();
-        gbc_mMaxAccelerationField.insets = new Insets(0, 0, 5, 0);
-        gbc_mMaxAccelerationField.fill = GridBagConstraints.HORIZONTAL;
-        gbc_mMaxAccelerationField.gridx = 1;
-        gbc_mMaxAccelerationField.gridy = 1;
-        add(mMaxAccelerationField, gbc_mMaxAccelerationField);
+        GridBagConstraints gbcMaxAccelerationField = new GridBagConstraints();
+        gbcMaxAccelerationField.insets = new Insets(0, 0, 5, 0);
+        gbcMaxAccelerationField.fill = GridBagConstraints.HORIZONTAL;
+        gbcMaxAccelerationField.gridx = 1;
+        gbcMaxAccelerationField.gridy = 1;
+        add(mMaxAccelerationField, gbcMaxAccelerationField);
         mMaxAccelerationField.setColumns(10);
 
         JLabel lblMaxJerk = new JLabel("Max Jerk");
-        GridBagConstraints gbc_lblMaxJerk = new GridBagConstraints();
-        gbc_lblMaxJerk.anchor = GridBagConstraints.EAST;
-        gbc_lblMaxJerk.insets = new Insets(0, 0, 5, 5);
-        gbc_lblMaxJerk.gridx = 0;
-        gbc_lblMaxJerk.gridy = 2;
-        add(lblMaxJerk, gbc_lblMaxJerk);
+        GridBagConstraints gbcLblMaxJerk = new GridBagConstraints();
+        gbcLblMaxJerk.anchor = GridBagConstraints.EAST;
+        gbcLblMaxJerk.insets = new Insets(0, 0, 5, 5);
+        gbcLblMaxJerk.gridx = 0;
+        gbcLblMaxJerk.gridy = 2;
+        add(lblMaxJerk, gbcLblMaxJerk);
 
         mMaxJerkField = new JTextField();
-        GridBagConstraints gbc_mMaxJerkField = new GridBagConstraints();
-        gbc_mMaxJerkField.insets = new Insets(0, 0, 5, 0);
-        gbc_mMaxJerkField.fill = GridBagConstraints.HORIZONTAL;
-        gbc_mMaxJerkField.gridx = 1;
-        gbc_mMaxJerkField.gridy = 2;
-        add(mMaxJerkField, gbc_mMaxJerkField);
+        GridBagConstraints gbcMaxJerkField = new GridBagConstraints();
+        gbcMaxJerkField.insets = new Insets(0, 0, 5, 0);
+        gbcMaxJerkField.fill = GridBagConstraints.HORIZONTAL;
+        gbcMaxJerkField.gridx = 1;
+        gbcMaxJerkField.gridy = 2;
+        add(mMaxJerkField, gbcMaxJerkField);
         mMaxJerkField.setColumns(10);
 
         JLabel lblIsBackwards = new JLabel("Is Backwards");
-        GridBagConstraints gbc_lblIsBackwards = new GridBagConstraints();
-        gbc_lblIsBackwards.insets = new Insets(0, 0, 0, 5);
-        gbc_lblIsBackwards.gridx = 0;
-        gbc_lblIsBackwards.gridy = 3;
-        add(lblIsBackwards, gbc_lblIsBackwards);
+        GridBagConstraints gbcLblIsBackwards = new GridBagConstraints();
+        gbcLblIsBackwards.insets = new Insets(0, 0, 0, 5);
+        gbcLblIsBackwards.gridx = 0;
+        gbcLblIsBackwards.gridy = 3;
+        add(lblIsBackwards, gbcLblIsBackwards);
 
         mIsBackwardsBox = new JCheckBox("");
-        GridBagConstraints gbc_mIsBackwardsBox = new GridBagConstraints();
-        gbc_mIsBackwardsBox.gridx = 1;
-        gbc_mIsBackwardsBox.gridy = 3;
-        add(mIsBackwardsBox, gbc_mIsBackwardsBox);
+        GridBagConstraints gbcIsBackwardsBox = new GridBagConstraints();
+        gbcIsBackwardsBox.gridx = 1;
+        gbcIsBackwardsBox.gridy = 3;
+        add(mIsBackwardsBox, gbcIsBackwardsBox);
     }
 
 }
