8f82b7b990161d1f9e9c88e347e0c93158754924
PJ Reiniger <pj.reiniger@gmail.com>
2022-01-22 17:54:50 -0500

    Making directories consistent

--- CoordinateGuiWidget2019/src/main/java/org/snobot/coordinate_gui/shuffleboard/deep_space/CoordinateGuiWidget2019.java
+++ CoordinateGuiWidget2019/src/main/java/org/snobot/coordinate_gui/shuffleboard/deep_space/CoordinateGuiWidget2019.java
@@ -1,9 +1,8 @@
-
-package org.snobot.coordinate_gui.shuffleboard;
+package org.snobot.coordinate_gui.shuffleboard.deep_space;
 
 import org.snobot.coordinate_gui.game.deep_space.DeepSpaceController;
 import org.snobot.coordinate_gui.model.Distance;
-import org.snobot.coordinate_gui.shuffleboard.data.DeepSpaceCoordinateGuiData;
+import org.snobot.coordinate_gui.shuffleboard.deep_space.data.DeepSpaceCoordinateGuiData;
 import org.snobot.coordinate_gui.shuffleboard.widgets.BaseCoordinateGuiWidget;
 
 import edu.wpi.first.shuffleboard.api.widget.Description;
